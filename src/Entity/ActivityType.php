5fbd50e4a4ca0b0a60ed0605a6cbbc7e2fdd3e11
Danel Rico <danelricofer@gmail.com>
2025-01-29 18:56:32 +0100

    linkeada la Base de datos y empezando a hacer los entity

---
+++ src/Entity/ActivityType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActivityTypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActivityTypeRepository::class)]
+class ActivityType
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $number_monitors = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumberMonitors(): ?int
+    {
+        return $this->number_monitors;
+    }
+
+    public function setNumberMonitors(int $number_monitors): static
+    {
+        $this->number_monitors = $number_monitors;
+
+        return $this;
+    }
+}
